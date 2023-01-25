47f641555f8db49576475cbfd3470864f03090bc
Bruno Adriano <bruno@mbp2020rkn.local>
2023-01-25 10:12:26 +0900

    more updates

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+# here = os.path.abspath(os.path.dirname("__file__"))
+
+# What packages are required for this module to be executed?
+try:
+    with open(os.path.join(here, "requirements.txt"), encoding="utf-8") as f:
+        REQUIRED = f.read().split("\n")
+except:
+    REQUIRED = []
+
+setup(
+    name="open_earth_map",
+    version="0.1",
+    description="Demo code for the OpenEarthMap project",
+    author="Bruno Adriano",
+    author_email="bruno.adriano@riken.jp",
+    packages=["open_earth_map"],
+    install_requires=REQUIRED,
+)

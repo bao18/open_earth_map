39adc008354eb0258c3b1920a7c00ff82a10cbbf
Bruno Adriano <bruno@mbp2020rkn.local>
2023-01-25 10:05:39 +0900

    more updates

--- demo_training.py
+++ demo_training.py
@@ -3,7 +3,8 @@ import time
 import warnings
 import numpy as np
 import torch
-import open_earth_map.oem as oem
+# import open_earth_map.oem as oem
+import oem
 import torchvision
 from pathlib import Path
 
